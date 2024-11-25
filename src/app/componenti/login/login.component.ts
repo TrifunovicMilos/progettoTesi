aa6d00c99c3576055b391fb3c6cda208be76c1c2
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-11-25 09:02:44 +0100

    Nascondi/mostra password Login/Registrazione

--- src/app/componenti/login/login.component.ts
+++ src/app/componenti/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -10,53 +11,98 @@ import { AuthService } from '../../auth/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ForgotPasswordDialogComponent } from '../forgot-password-dialog/forgot-password-dialog.component';
 import { passwordValidator } from '../../validators/password.validator';
+import { confirmPasswordValidator } from '../../validators/confirmPassword.validator';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatTabsModule, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [ MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, MatTabsModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   registerForm!: FormGroup;
   loginForm!: FormGroup;
-  // salvo in una variabile la email con la quale ho provato ad accedere, 
+  // salvo in una variabile la email con la quale ho provato ad accedere,
   // in modo tale da usarla come predefinita al click di "Password Dimenticata"
-  emailLogin = ''; 
+  emailLogin = '';
   registerPasswordError = 0; // tipologia errore password di registrazione
+  confirmPasswordError = 0;
+  passwordsMatch = true;
+  hideLoginPassword = true;
+  hideRegistrationPassword = true;
 
   constructor(private authService: AuthService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
     });
 
     this.registerForm = new FormGroup({
       email: new FormControl('', [Validators.required, 
-      Validators.pattern(/^[a-z]+(\.[a-z]+)+\.(studente|docente)@yopmail\.com$/)]),
-      
+        Validators.pattern(/^[a-z]+(\.[a-z]+)+\.(studente|docente)@yopmail\.com$/),]),
+
       // La password deve contenere almeno 8 caratteri, una lettera maiuscola, una lettera minuscola e un numero.
-      password: new FormControl('', [Validators.required, Validators.minLength(8),
-      passwordValidator()]),
-      confirmPassword: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8), passwordValidator()]),
+      confirmPassword: new FormControl(''),
     });
 
     this.registerForm.get('password')?.valueChanges.subscribe(() => {
       this.checkPasswordErrors();
+      this.comparePasswords();
+    });
+
+    this.registerForm.get('confirmPassword')?.setValidators([
+      Validators.required, confirmPasswordValidator(this.registerForm.get('password')!),
+    ]);
+
+    this.registerForm.get('confirmPassword')?.valueChanges.subscribe(() => {
+      this.checkConfirmPasswordsErrors();
+      this.comparePasswords();
     });
   }
 
-  checkPasswordErrors() {
-      if(this.registerForm.get('password')?.hasError('required'))
-      this.registerPasswordError = 1 // password non inserita
-      else if(this.registerForm.controls['password'].hasError('minlength'))
-      this.registerPasswordError = 2 // password corta
-      else if(this.registerForm.controls['password'].hasError('passwordStrength'))
-      this.registerPasswordError = 3 // password debole
+  toggleLoginPassword(): void {
+    this.hideLoginPassword = !this.hideLoginPassword;
+  }
+
+  toggleRegistrationPassword(): void {
+    this.hideRegistrationPassword = !this.hideRegistrationPassword;
+  }
+
+  private checkPasswordErrors(): void {
+    const password = this.registerForm.get('password')
+
+    if (password?.hasError('required'))
+      this.registerPasswordError = 1; // password non inserita
+    else if (password?.hasError('minlength'))
+      this.registerPasswordError = 2; // password corta
+    else if (password?.hasError('passwordStrength'))
+      this.registerPasswordError = 3; // password debole
+    else {
+      this.registerPasswordError = 0;
+    }
+  }
+
+  private checkConfirmPasswordsErrors(): void {
+    const confirmPassword = this.registerForm.get('confirmPassword');
+
+    if (confirmPassword?.hasError('required'))
+      this.confirmPasswordError = 1; 
+    else if (confirmPassword?.hasError('passwordMismatch'))
+      this.confirmPasswordError = 2; 
+    else {
+      this.confirmPasswordError = 0;
+    }
+  }
+  
+  // aggiorna passwordMatch ogni volta che password o confirmPassword subiscono modifiche
+  private comparePasswords(): void {
+    this.passwordsMatch =
+      this.registerForm.get('confirmPassword')?.value ==
+      this.registerForm.get('password')?.value;
   }
 
   onLoginSubmit(): void {
@@ -64,63 +110,53 @@ export class LoginComponent implements OnInit{
       const { email, password } = this.loginForm.value; // estraggo i valori dal form
       this.emailLogin = email;
 
-      this.authService.login(email, password).then(() => {
-        console.log('Login eseguito');
-      }).catch((error: any) => {
-        if(error.message === 'Email non verificata. '){
-          console.log('Email non verificata');
-          alert(error.message + 'Controlla la tua posta.')
-        }
-        else{
-          console.log('Errore durante il login');
-          alert("Errore durante il login. Controlla le credenziali.");
-        }
-      });
+      this.authService.login(email, password)
+        .then(() => {
+          console.log('Login eseguito');
+        })
+        .catch((error: any) => {
+          if (error.message === 'Email non verificata. ') {
+            console.log('Email non verificata');
+            alert(error.message + 'Controlla la tua posta.');
+          } else {
+            console.log('Errore durante il login');
+            alert('Errore durante il login. Controlla le credenziali.');
+          }
+        });
     }
   }
 
   openForgotPasswordDialog(): void {
     const dialogRef = this.dialog.open(ForgotPasswordDialogComponent, {
       width: '400px',
-      data: { email: this.emailLogin } // Passo l'email al dialog
+      data: { email: this.emailLogin }, // Passo l'email al dialog
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('Il dialog è stato chiuso');
     });
   }
 
-  // chiamata da onRegisterSubmit()
-  private passwordsMatch(): boolean {
-    return (
-      this.registerForm.get('confirmPassword')?.value ==
-      this.registerForm.get('password')?.value
-    );
-  }
-
   onRegisterSubmit(): void {
-    if (!this.passwordsMatch()) {
-      alert('Le password non corrispondono');
-    } else {
-      const email = this.registerForm.value.email;
-      const password = this.registerForm.value.password;
+    const email = this.registerForm.value.email;
+    const password = this.registerForm.value.password;
 
-      this.authService.signUp(email, password).then(()=> {
-          alert('Registrazione completata! Controlla la tua email per verificare l’account.');
-          console.log('Utente registrato con successo');
-          // una volta registrati si va su /signin. 
-          //Usando router.navigate(['/signin']) al posto del reload non funziona perché ci fa rimanere sulla tab 'Registrazione'
-          window.location.reload();
-        })
-        .catch((error: any) => {
-          if (error.code === 'auth/email-already-in-use') {
-            console.log('Email già registrata')
-            alert("Questa email è già registrata. Per favore, prova a fare il login.");
-          } else {
-            console.error(error);
-            alert('Si è verificato un errore durante la registrazione.');
-          }
+    this.authService.signUp(email, password)
+      .then(() => {
+        alert('Registrazione completata! Controlla la tua email per verificare l’account.');
+        console.log('Utente registrato con successo');
+        // una volta registrati si va su /signin.
+        //Usando router.navigate(['/signin']) al posto del reload non funziona perché ci fa rimanere sulla tab 'Registrazione'
+        window.location.reload();
+      })
+      .catch((error: any) => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('Email già registrata');
+          alert('Questa email è già registrata. Per favore, prova a fare il login.');
+        } else {
+          console.error(error);
+          alert('Si è verificato un errore durante la registrazione.');
+        }
       });
-    }
   }
 }
