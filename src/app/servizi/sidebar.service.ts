449abac3f696a56ec4393876be543835e533f05e
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-11-26 16:52:37 +0100

    Ignore

--- src/app/servizi/sidebar.service.ts
+++ src/app/servizi/sidebar.service.ts
@@ -6,20 +6,20 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class SidebarService {
 
-  private sidebarState = new BehaviorSubject<boolean>(false); // Stato iniziale aperto
+  private sidebarState = new BehaviorSubject<boolean>(false); // inizialmente chiusa (stretta)
   sidebarState$ = this.sidebarState.asObservable();
 
   toggleSidebar() {
     const currentState = this.sidebarState.value;
-    this.sidebarState.next(!currentState); // Cambia lo stato
+    this.sidebarState.next(!currentState); // cambia stato sidebar ad ogni click sul menu
   }
 
   getSidebarState() {
-    return this.sidebarState.value; // Stato attuale
+    return this.sidebarState.value; // stato attuale
   }
 
   setSidebarState(state: boolean) {
-    this.sidebarState.next(state);
+    this.sidebarState.next(state); // chiamato dalle modiche alla sidebar tramite mouse enter/leave
   }
   
 }
