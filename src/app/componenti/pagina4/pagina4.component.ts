f7c7a98de6fe86b7b54f9b4da40496089b5e0902
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-11-29 09:36:35 +0100

    Ignore ...

---
+++ src/app/componenti/pagina4/pagina4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina4',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina4.component.html',
+  styleUrl: './pagina4.component.css'
+})
+export class Pagina4Component {
+
+}
