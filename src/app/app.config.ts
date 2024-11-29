00ba802f0b4b5b66fbad7ab0151340cc3be2de77
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-11-29 11:47:40 +0100

    Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,10 @@ import { provideHttpClient } from '@angular/common/http';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideFirebaseApp } from '@angular/fire/app';
-import { firebaseConfig } from '../environments/firebase.config';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore())]
 };
--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDC5n7iWZK43g6NixBX7SbS-rs3akW-DOk",
-    authDomain: "progetto-tesi-4278e.firebaseapp.com",
-    projectId: "progetto-tesi-4278e",
-    storageBucket: "progetto-tesi-4278e.firebasestorage.app",
-    messagingSenderId: "950886463622",
-    appId: "1:950886463622:web:6f3a542be11d8acea9f907"
-  };
