13a1fd9d58b07fd7b971c9338b318a965ef182f0
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-11-30 21:22:33 +0100

    Ignore ...

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -29,18 +29,8 @@ export const routeTransition = trigger('routeTransition', [
   ]);
 
   export const slideInAnimation = trigger('slideInAnimation', [
-
-    transition('* <=> *', [
-      query(':enter, :leave', style({ position: 'fixed', width: '100%', zIndex: 2 }), { optional: true }),
-      group([
-        query(':enter', [
-          style({ transform: 'translateX(100%)' }),
-          animate('0.5s ease-out', style({ transform: 'translateX(0%)' }))
-        ], { optional: true }),
-        query(':leave', [
-          style({ transform: 'translateX(0%)' }),
-          animate('0.5s ease-out', style({ transform: 'translateX(-100%)' }))
-        ], { optional: true })
-      ])
+    transition('* => *', [
+      style({ opacity: 0, transform: 'translateX(100%)' }),  // Inizia fuori dalla vista
+      animate('0.5s ease-in', style({ opacity: 1, transform: 'translateX(0)' }))  // Muove l'elemento nella vista
     ])
   ]);
