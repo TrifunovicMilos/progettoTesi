1bc3fc512967e4cadf0c11cf568f386b488e36b3
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-12-13 12:20:29 +0100

    Ignore...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './componenti/login/login.component';
 import { DashboardComponent } from './componenti/dashboard/dashboard.component';
 import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './componenti/home/home.component';
-import { Pagina2Component } from './componenti/pagina2/pagina2.component';
 import { Pagina3Component } from './componenti/pagina3/pagina3.component';
 import { ProfiloComponent } from './componenti/profilo/profilo.component';
 import { Pagina4Component } from './componenti/pagina4/pagina4.component';
@@ -18,14 +17,14 @@ export const routes: Routes = [
     // Dashboard contiene Header (in alto) e Sidebar (a sinistra) fissi 
     // e, a destra, una delle pagine sottostanti (<routeroutlet> in dashboard.component.ts)
     {path: '', component: DashboardComponent, canActivate: [authGuard], children: [
-        {path:'', redirectTo: 'home', pathMatch: 'full'},
-        {path:'home', component: HomeComponent},
+        {path: '', redirectTo: 'home', pathMatch: 'full'},
+        {path: 'home', component: HomeComponent},
         {path: 'esami/:id', component: EsameComponent },
         {path: 'esami/:idEsame/domande', component: DomandeComponent},
-        {path:'my-exams', component: IMieiEsamiComponent},
-        {path:'pagina3', component: Pagina3Component},
-        {path:'pagina4', component: Pagina4Component},
-        {path:'profilo', component: ProfiloComponent},
+        {path: 'my-exams', component: IMieiEsamiComponent},
+        {path: 'pagina3', component: Pagina3Component},
+        {path: 'pagina4', component: Pagina4Component},
+        {path: 'profilo', component: ProfiloComponent},
     ]},
     {path: 'login', component: LoginComponent},
     {path: '404', component: NotfoundComponent},
