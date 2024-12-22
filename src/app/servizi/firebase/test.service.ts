7d9782aebc73366c8639eb23e2bfa1bd1221ed8b
TrifunovicMilos <milostrifunovic02@yahoo.it>
2024-12-22 14:33:21 +0100

    funzione creazione tipoTest

--- src/app/servizi/firebase/test.service.ts
+++ src/app/servizi/firebase/test.service.ts
@@ -11,6 +11,35 @@ export class TestService {
   private firestore: Firestore = inject(Firestore);
   constructor() { }
 
+  async createTipoTest(nomeTest: string, descrizione: string, poolId: string, numeroDomande: number, esameId: string): Promise<any> {
+    const esamiColRef = collection(this.firestore, 'esami');
+    const tipiTestColRef = collection(this.firestore, 'tipiTest');
+
+    // 1. Creazione nuovo documento nella collezione "tipiTest" 
+    const tipoTestDocRef = await addDoc(tipiTestColRef, {
+      nomeTest: nomeTest,
+      descrizione: descrizione,
+      pool: poolId,
+      numeroDomande: numeroDomande,
+    });
+    
+    // 2. Aggiunta dell'ID del tipoTest al documento dell'esame
+    const esameDocRef = doc(this.firestore, 'esami', esameId);
+    const esameDocSnap = await getDoc(esameDocRef);
+
+    if (esameDocSnap.exists()) {
+      const existingPools = esameDocSnap.data()['tipiTest'] || [];
+      await updateDoc(esameDocRef, {
+        tipiTest: [...existingPools, tipoTestDocRef.id],
+      });
+
+      // console.log(`Pool ${poolDocRef.id} aggiunto all'esame ${esameId}`);
+    } else {
+      throw new Error('Esame non trovato.');
+    }
+
+  }
+
   async getTipoTestById(id: string): Promise<any> {
     const tipoTestDocRef = doc(this.firestore, 'tipiTest', id);
     const docSnap = await getDoc(tipoTestDocRef);
@@ -21,6 +50,8 @@ export class TestService {
       throw new Error('TipoTest non trovata.');
     }
   }
+
+
 
 
 }
